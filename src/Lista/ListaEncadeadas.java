89f02aba5acdcf525248fef08a778a3cd8227873
otavio <otaviocarmo21@gmail.com>
2026-04-13 11:28:05 -0300

    Atualizando repositorio

--- src/Lista/ListaEncadeadas.java
+++ src/Lista/ListaEncadeadas.java
@@ -4,6 +4,7 @@ public class ListaEncadeadas<T> {
     private No<T> inicio;
     private No<T> fim;
     private int tamanho = 0;
+    private final int NAO_ENCONTRADO = -1;
 
     public int getTamanho() {
         return tamanho;
@@ -61,5 +62,137 @@ public class ListaEncadeadas<T> {
         sb.append("]");
 
         return sb.toString();
+    }
+
+    public int buscar(T elemento) {
+        No<T> atual = this.inicio;
+        int posicao = 0;
+
+        while (atual != null) {
+            if (atual.getElemento().equals(elemento)) {
+                return posicao;
+            }
+            posicao ++;
+            atual = atual.getProximo();
+        }
+
+        return NAO_ENCONTRADO;
+    }
+
+    private No<T> buscaPorNo(int posicao) {
+        if (!(posicao >= 0 && posicao < this.tamanho)) {
+            throw new IndexOutOfBoundsException("Posição inválido");
+        }
+
+        No<T> atual = this.inicio;
+
+        for (int i = 0; i < posicao; i++) {
+            atual = atual.getProximo();
+        }
+
+        return atual;
+    }
+
+    public T buscarPosicao(int posicao) {
+        return this.buscaPorNo(posicao).getElemento();
+    }
+
+    public void adicionaInicio(T elemento) {
+        if (this.tamanho == 0) {
+            No<T> atual = new No<>(elemento);
+
+            this.inicio = atual;
+            this.fim = atual;
+
+        } else {
+            No<T> atual = new No<>(elemento);
+
+            atual.setProximo(this.inicio);
+            this.inicio =  atual;
+        }
+
+        this.tamanho++;
+    }
+
+    public void adicionar(T elemento, int posicao) {
+        if (!(posicao >= 0 && posicao < this.tamanho)) {
+            throw new IndexOutOfBoundsException("Posição inválido.");
+        }
+
+        if (posicao == 0) {
+
+            this.adicionaInicio(elemento);
+
+        } else if (posicao == this.tamanho) {
+
+            this.adicionar(elemento);
+
+        } else {
+            No<T> anterior = this.buscaPorNo(posicao - 1);
+            No<T> proximo =  anterior.getProximo();
+
+            No<T> atual = new No<>(elemento, proximo);
+
+            anterior.setProximo(atual);
+            this.tamanho++;
+        }
+    }
+
+    public T removerInicio() {
+        if (this.tamanho == 0) {
+            throw new RuntimeException("A lista está vazia.");
+        }
+
+        T removido = this.inicio.getElemento();
+
+        this.inicio = this.inicio.getProximo();
+        this.tamanho--;
+
+        if (this.tamanho == 0) {
+            this.fim = null;
+        }
+
+        return removido;
+    }
+
+    public T removerFinal() {
+        if (this.tamanho == 0) {
+            throw new RuntimeException("A lista está vazia.");
+        }
+
+        if (this.tamanho == 1) {
+            this.removerInicio();
+        }
+
+        No<T> penultimo = this.buscaPorNo(this.tamanho - 2);
+        T  removido = penultimo.getProximo().getElemento();
+
+        penultimo.setProximo(null);
+        this.fim = penultimo;
+
+        this.tamanho--;
+        return removido;
+    }
+
+    public T remover(int posicao) throws IllegalAccessException {
+        if (posicao < 0 || posicao > this.tamanho) {
+            throw new IllegalAccessException("Posição inválida");
+        }
+
+        if  (posicao == 0) {
+            this.removerInicio();
+        } if (posicao == tamanho - 1) {
+            this.removerFinal();
+        }
+
+        No<T> aterior = this.buscaPorNo(posicao - 1);
+        No<T> atual =  aterior.getProximo();
+        No<T> proximo =  atual.getProximo();
+
+        aterior.setProximo(proximo);
+        atual.setProximo(null);
+
+        this.tamanho--;
+        return atual.getElemento();
     }
 }
--- src/Lista/Teste/Teste.java
+++ src/Lista/Teste/Teste.java
@@ -3,16 +3,36 @@ package Lista.Teste;
 import Lista.ListaEncadeadas;
 
 public class Teste {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         ListaEncadeadas<Integer> lista = new ListaEncadeadas<>();
 
         lista.adicionar(9);
         lista.adicionar(8);
         lista.adicionar(7);
+        // lista.adicionaInicio(6);
+        lista.adicionar(6, 2);
 
         System.out.println(lista);
 
-        lista.limpa();
-        System.out.println(lista);
+        //lista.limpa();
+
+        System.out.println("Buscando pelo elemento e mostrando o indice: " + lista.buscar(9));
+        System.out.println("Buscando pela possição e mostrando o elemento: " + lista.buscarPosicao(2));
+
+        System.out.println();
+
+        ListaEncadeadas<Integer> lista2 = new ListaEncadeadas<>();
+
+        lista2.adicionaInicio(9);
+        lista2.adicionaInicio(10);
+        lista2.adicionaInicio(11);
+        lista2.adicionaInicio(12);
+        lista2.adicionaInicio(13);
+
+        // lista2.removerInicio();
+        // lista2.removerFinal();
+        lista2.remover(2);
+
+        System.out.println(lista2);
     }
 }
